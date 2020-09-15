b91bf088d373040e5e8e15d1c7a53f04c3e846d4
janki000 <54926536+janki000@users.noreply.github.com>
2020-09-14 20:31:47 -0400

    Roche papier

--- js.js
+++ js.js
@@ -1,32 +1,14 @@
 const play = document.querySelector(".btn");
 
 play.addEventListener("click",() => {
+  const game = new Game();
   document.querySelector(".letout").classList.add("none");
   document.querySelector(".resultat").classList.remove("none")
   
-   const nom = document.querySelector(".nom").value
-  const player = document.querySelector(".player")
-  player.innerHTML=nom;
   
   
-  const rad0 = document.getElementsByName('peau');
-  const rad1 = document.getElementById('jaune');
-  const rad2 = document.getElementById('blanc');
-  const rad3 = document.getElementById('mulate');
-  const rad4 = document.getElementById('noir');
-  
-
-const choixir = document.querySelector(".choix");
-  console.log(choix)
-if (rad1.checked && choixir.value == 0) {
-      document.getElementId("joueur").innerHTML ="✊";
-  
-    }
-   
-  
- 
-  
-  
+   var nom = document.getElementById("nom").value;
+  document.getElementById("joueur").innerHTML=nom;
 })
 
 const rejouer = document.querySelector(".rejouer");
@@ -34,3 +16,76 @@ rejouer.addEventListener("click",() => {
   document.querySelector(".resultat").classList.add("none");
   document.querySelector(".letout").classList.remove("none");
 })
+
+
+
+class Game{
+  constructor(){
+    this.mainTab = [
+      ["✊","✋","✌️"],
+      ["✊🏻","✋🏻","✌🏻"],
+      ["✊🏽","✋🏽","✌🏽"],
+      ["✊🏿","✋🏿","✌🏿"],
+    ];
+    
+    this.choixJoueur();
+    this.afficherChoixOpposant();
+    this.resultatGame();
+  }
+  choixJoueur(){
+    const RpS = document.getElementById("choix");
+    const indexRpS = RpS.selectedIndex;
+    const choixPeau = document.querySelector('[name = peau]:checked').value;
+    const actionJoueur = this.mainTab[choixPeau][indexRpS];
+    
+    const resultatJoueur = document.getElementById("cJoueur");
+
+     resultatJoueur.innerHTML =`<span>${actionJoueur}</span>`;
+  }
+  
+   afficherChoixOpposant() {
+    const cAleatoire = Math.round(Math.random() * 3);
+    const cOrdi = this.mainTab[cAleatoire];
+    
+    this.actionRandom = Math.round(Math.random() * 2);
+    const mouvOrdi = cOrdi[this.actionRandom];
+    
+
+    const resultatOrdi = document.querySelector(".cOrdi");
+    resultatOrdi.innerHTML = `<span>${mouvOrdi}</span>`;
+}
+  
+  resultatGame() {
+    const coJoueur = document.querySelector('.player');
+    const coOrdi = document.querySelector('.player2');
+    const annonce = document.querySelector('.annonce');
+    
+   
+    
+    const  RpS = document.getElementById("choix");
+    const indexRpS = RpS.selectedIndex;
+    
+    
+    if(indexRpS == this.actionRandom){
+      coJoueur.style.setProperty('background-color', "#3F88C5");
+      coOrdi.style.setProperty('background-color', "#3F88C5");
+      annonce.innerHTML = "<span>ÉGALITÉ!</span>"; 
+    }
+
+    else if(
+      indexRpS == 0 && this.actionRandom == 1 ||
+      indexRpS == 1 && this.actionRandom == 2 ||
+      indexRpS == 2 && this.actionRandom == 0 ){
+        coJoueur.style.setProperty('background-color', "#D16D82");
+        coOrdi.style.setProperty('background-color', "#7FD8BE");
+        annonce.innerHTML = `<span>DÉFAITE!</span>`;
+      }
+    
+    else{
+      coJoueur.style.setProperty('background-color', "#7FD8BE");
+        coOrdi.style.setProperty('background-color', "#D16D82");
+        annonce.innerHTML = `<span>VICTOIRE!</span>`;
+    }
+  
+}
+  }
